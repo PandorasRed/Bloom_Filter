c735482c4abdd53ebb5dc0778beab9345b959900
Oceane <oceane.guinard@outlook.fr>
2022-01-18 14:38:21 +0100

    marche niquel(faut eviter de tenter avec une grosse taille par contre)

--- src/bloomFilter.cpp
+++ src/bloomFilter.cpp
@@ -14,21 +14,22 @@
         }
         //work but need a redo of constructor so need to be modified
         void  bloomFilter::add_value(uint64_t x){
-            x=x+1; // to be sure that 0 don't cause problem, just need to do the same for is_present
+            x++; // to be sure that 0 don't cause problem, just need to do the same for is_present
             uint64_t hashedCell[nbf];
             multihash(x,hashedCell,nbf,size-1);
             uint64_t tmp=0;
             int modtmp;
             for(unsigned int i=0;i<nbf;i++){
                 tmp=hashedCell[i];
-                modtmp=tmp%8;
-                if(modtmp==0){
-                    bytesArray[tmp-1/8]|=1;
-                    //std::cout<<tmp<<std::endl;
+                if(tmp==0){
+                    bytesArray[0]|=1<<7;
                 }else{
-                    bytesArray[tmp/8]|=1<<(8-modtmp);
-                    //std::cout<<tmp<<std::endl;
-
+                    modtmp=tmp%8;
+                    if(modtmp==0){
+                        bytesArray[(tmp-1)/8]|=1;
+                    }else{
+                        bytesArray[tmp/8]|=1<<(7-modtmp);
+                    }
 
                 }
             }
@@ -44,18 +45,23 @@
             for(unsigned int i=0;i<nbf;i++){
                 tmp=hashedCell[i];
                 modtmp=tmp%8;
-                if(modtmp==0){
-                    if((bytesArray[tmp-1/8]&1)!=1){//not sure its work so need more test;
-                        return 0;
-                    }
+                if(tmp==0){
+
                 }else{
-                    int b=(bytesArray[tmp/8]&(1<<(8-modtmp)));//read the bit we want;
-                    b=b>>(8-modtmp);//shift it to make it a 1 or a 0
-                    if(b!=1){ 
-                        return 0;
+                    if(modtmp==0){
+                        if((bytesArray[(tmp-1)/8]&1)!=1){
+                            return 0;
+                        }
+                    }else{
+                        int b=(bytesArray[tmp/8]&(1<<(7-modtmp)));//read the bit we want;
+                        b=b>>(7-modtmp);//shift it to make it a 1 or a 0
+                        if(b!=1){ 
+                            return 0;
                         
+                        }
                     }
                 }
+               
             }
             return 1;
         }
--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,27 @@
  
 int main(int argc, char *argv[])
 {
+	/*std::vector<bool>*  vt=new std::vector<bool>(10,0);
+	vt->resize(50000);
+	std::cout<<vt->at(48546) << std::endl;
+	vt->at(48546)=1;
+	std::cout<<vt->at(48546) << std::endl;
+	delete vt;*/
+	/*uint8_t test=0;
+	test|=(1<<7);
+	std::cout<<(unsigned int)test<<std::endl;*/
 	if(argc==6){
 		std::cout << "nombre d'argument invalide, la syntaxe du programme est main p t nbh nbt "<< std::endl;
 		std::cout << "p le path du fichier fasta;\nt la taille du filtre de bloom demander(max2^34);\nnbh le nombre de fonction de hashage demander;\nnbt le nombre de requette de test;\n";
 	}else{
 		srand (time(NULL));
-		bloomFilter* a=new bloomFilter(200,5);
+		bloomFilter* a=new bloomFilter((unsigned long int)2000,5);
 		std::ifstream fs(argv[1]);//fait le ifstream du fichier
 		firstSuppr(fs);
 		std::string t=newkmer("",5,fs);
 		t=reverseComplement(t,5);
+		a->add_value(hashKmer(t,5));
+
 		while(t!="E"){
 			t=newkmer(t,5,fs);
 			t=reverseComplement(t,5);
@@ -21,10 +32,9 @@ int main(int argc, char *argv[])
 				a->add_value(hashKmer(t,5));
 			}
 		}
-
+		
 		fs.close();
 		delete a;
-		//std::cout<<b<<std::endl;
 		for(int i=0;i<10;i++){
 			std::cout<<a->is_present(hashKmer(reverseComplement(randomkmer(5),5),5)) << std::endl;
 		}
